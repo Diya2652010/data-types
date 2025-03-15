7a6aa7838c2db937fecd22cf444345c03ec1dad1
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-15 17:52:15 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+first_name="codingal"
+last_name="educations"
+full_name="first_name"+"last_name"
+example="HA"*6
+
+print("first name:",first_name)
+print("last name:",last_name)
+print("full name:",full_name)
+print("string multiplied 5 times gives the result:",example)
+
+word="codingal"
+print("length of string:",len(word))
+print("first letter of string:",word[0])
+print("last letter of string:",word[5])
+print("string sliced:",word[0:3])
