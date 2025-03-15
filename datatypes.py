3ce94902eb28f4d62193e04341611692c555c98c
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-15 17:19:54 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+name="diya.M"
+age=14
+is_student=True
+weight=47.2
+
+print("name:",name)
+print("Data type of name is:",type(name))
+print("age:",age)
+print("Data type of age is:",type(age))
+print("is_student:",is_student)
+print("Data type of is_student is:",type(is_student))
+print("weight:",weight)
+print("Data type of weight is:",type(weight))
+
+print("/n after tpe casting...")
+age=str(age)
+print(age)
+print("data type od age is",type(age))
+weight=int(weight)
+print("datatype of weight is",type(weight))
