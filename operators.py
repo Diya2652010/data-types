671c00196978cf804f60f7cd46459c3a06e81c82
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-15 17:40:58 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,21 @@
+num1=10
+num2=7
+
+print("number 1",num1)
+print("number 2",num2)
+print("addition:",num1+num2)
+print("difference:",num1-num2)
+print("product:",num1*num2)
+print("division:",num1/num2)
+print("floor division:",num1//num2)
+print("modulus operators:",num1%num2)
+print("square:",num2**2)
+print("square root:",num1**0.5)
+
+print("equal?",num1==num2)
+print("numbert 1 greater?",num1>num2)
+print("numbert 2 greater?",num1<num2)
+print("not equal?",num1!=num2)
+
+result=num1/2 + num2**2+10
+print("result of given equation is:",result)
