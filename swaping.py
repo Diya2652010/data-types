7527290d9f552a8b4e721d04cf6e4827c1077094
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-22 10:15:56 +0530

    swaping

---
+++ swaping.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+temp=x
+x=y
+y=temp
+
+print("The value of the first number after sawping:",x)
+print("The value of the second number after sawping:",y)
